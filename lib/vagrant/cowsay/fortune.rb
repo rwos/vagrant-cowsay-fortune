0329bae4f350e2cb4690cf708f6b613e190d54c9
Richard Wossal <richard@r-wos.org>
2014-05-12 23:43:42 +0200

    shotgun debugging: copy random stuff from bib-vagrant

--- lib/vagrant/cowsay/fortune.rb
+++ lib/vagrant/cowsay/fortune.rb
@@ -1,11 +1,17 @@
+begin
+  require "vagrant"
+rescue LoadError
+  raise "This is a vagrant plugin, do not use standalone."
+end
 require "vagrant/cowsay/fortune/version"
 require "ext/string"
 
-module Vagrant
+module VagrantPlugins
   module Cowsay
     module Fortune
-      class VagrantCowsayFortune < Vagrant.plugin("2")
+      class Plugin < Vagrant.plugin("2")
         name "vagrant-cowsay-fortune"
+        description "Cowsay + Fortune"
         'fortune'.works? and 'cowsay'.works? and system('fortune -a | cowsay')
       end
     end
